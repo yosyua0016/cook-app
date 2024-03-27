0c77c106bd4266b0cc8625fd7a17f08e80facc06
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-03-28 00:54:28 +0900

    index.css.ok

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
   private
 
   def set_post
-    @post = current_user.posts.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def post_params
