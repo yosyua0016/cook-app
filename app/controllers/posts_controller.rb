52f399ea9ee95510d0b15e3cd6147215fe04e1d0
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-03-27 14:36:53 +0900

    写真投稿

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,47 @@
+# controllers/posts_controller.rb
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, except: [:index]
+
   def index
-    @posts = Post.all # すべての投稿を取得する例
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "投稿が作成されました。"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path, notice: "投稿が更新されました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+    redirect_to root_path, notice: "投稿が削除されました。"
   end
 
   private
 
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  def post_params
+    params.require(:post).permit(:title, :category_id, :image, :comment).merge(user_id: current_user.id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+ 
+ 
+         # Password format validation
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+
+ # Common validations
+  with_options presence: true do
+    validates :name
+  end
+
+  def username
+    name
+  end
+ has_many :posts
 end
