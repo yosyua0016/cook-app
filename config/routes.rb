fee741253d3e412ad47b4319e9825f4581313abd
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-03-29 10:50:02 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
   get 'posts/index'
   root "posts#index"
   
-  #root "users#show"
-  #as :user do
-   # get '/users/sign_out', to: 'devise/sessions#destroy', as: :logout
-  #end
   resources :users, only: [:show] # ユーザーのプロフィールページへのルートを追加
   resources :posts
 end
